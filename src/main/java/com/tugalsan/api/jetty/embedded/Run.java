5a80478e698c8284b850068de9cbb18d26fb6eca
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-29 13:36:38 +0300

    Update Run.java

--- src/main/java/com/tugalsan/api/jetty/embedded/Run.java
+++ src/main/java/com/tugalsan/api/jetty/embedded/Run.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 public class Run {
 
     public static void main(String[] args) throws Exception {
-        TS_JettyServer.main(8443, Path.of("D:\\xampp_data\\SSL\\tomcat.p12"), "MyPass");
+        TS_JettyServer.main(8443, Path.of("D:\\dat\\SSL\\tomcat.p12"), "MyPass");
     }
 
 }
